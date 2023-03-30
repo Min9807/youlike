57160c2b7aa27c42ab4b142df69c97a9bf8fddc9
Jeongmin Lee <dlwogns9807@naver.com>
2023-03-30 12:41:10 +0900

    세팅

---
+++ src/main/java/com/ll/gramgram/GramgramApplication.java
@@ -0,0 +1,13 @@
+package com.ll.gramgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GramgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GramgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/gramgram/GramgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.gramgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GramgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
