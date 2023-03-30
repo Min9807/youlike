464ab39516e9f7451e33bc3d0644b82745ce8145
Jeongmin Lee <dlwogns9807@naver.com>
2023-03-30 12:53:09 +0900

    회원가입 폼 테스트케이스

---
+++ src/main/java/com/ll/gramgram/boundedContext/member/controller/MemberController.java
@@ -0,0 +1,16 @@
+package com.ll.gramgram.boundedContext.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+    @GetMapping("/join")
+    public String showJoin() {
+        return "usr/member/join";
+    }
+}
---
+++ src/test/java/com/ll/gramgram/boundedContext/member/controller/MemberControllerTests.java
@@ -0,0 +1,40 @@
+package com.ll.gramgram.boundedContext.member.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest // 스프링부트 관련 컴포넌트 테스트할 때 붙여야 함, Ioc 컨테이너 작동시킴
+@AutoConfigureMockMvc // http 요청, 응답 테스트
+@Transactional // 실제로 테스트에서 발생한 DB 작업이 영구적으로 적용되지 않도록, test + 트랜잭션 => 자동롤백
+@ActiveProfiles("test") // application-test.yml 을 활성화 시킨다.
+public class MemberControllerTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("회원가입 폼")
+    void t001() throws Exception {
+        // WHEN
+        ResultActions resultActions = mvc
+                .perform(get("/member/join"))
+                .andDo(print()); // 크게 의미 없고, 그냥 확인용
+
+        // THEN
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showJoin"))
+                .andExpect(status().is2xxSuccessful());
+    }
+}
