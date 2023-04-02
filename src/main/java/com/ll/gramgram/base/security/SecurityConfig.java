e34793277506d2b9d0a6ff39aa8a0b0cfb12e32e
Jeongmin Lee <dlwogns9807@naver.com>
2023-04-02 20:49:16 +0900

    카카오 로그인 버튼

--- src/main/java/com/ll/gramgram/base/security/SecurityConfig.java
+++ src/main/java/com/ll/gramgram/base/security/SecurityConfig.java
@@ -20,6 +20,10 @@ public class SecurityConfig {
                         formLogin -> formLogin
                                 .loginPage("/member/login")
                 )
+                .oauth2Login(
+                        oauth2Login -> oauth2Login
+                                .loginPage("/member/login")
+                )
                 .logout(
                         logout -> logout
                                 .logoutUrl("/member/logout")
